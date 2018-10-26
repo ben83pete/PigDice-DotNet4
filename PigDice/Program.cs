daf0747b00d4a72ed3bfa8f496fde5acb41d83ab
Greg Doud <gpdoud@gmail.com>
2018-10-26 13:24:59 -0400

    clean-up disk

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -13,14 +13,15 @@ namespace PigDice {
 			return rnd.Next(1, 7);
 		}
 
-		void PlayPigDice() {
+		int PlayPigDice() {
 			int score = 0;
 			int die = RollDie();
 			while(die != 1) {
 				score += die;
 				die = RollDie();
 			}
-			Console.WriteLine("Score is {0}", score);
+			//Console.WriteLine("Score is {0}", score);
+			return score;
 		}
 
 		void Run() {
@@ -41,8 +42,46 @@ namespace PigDice {
 			}
 			Console.WriteLine($"Score is {score}");
 		}
+		void RunForBootcampRecord(string taskName) {
+			int BestScoreSoFar = 0;
+			long GameCount = 0;
+			while (true) {
+				int score = PlayPigDice();
+				GameCount++;
+				if (score > BestScoreSoFar) {
+					BestScoreSoFar = score;
+					Display(taskName, GameCount, BestScoreSoFar);
+				}
+				if (++GameCount % 1000000000 == 0)
+					Display(taskName, GameCount, BestScoreSoFar);
+			}
+		}
+		void Display(string taskName, long games, int score) {
+			Console.WriteLine($"Task: {taskName}; Game: {games.ToString("N0")}; Score: {score.ToString("N0")}");
+			System.Diagnostics.Debug.WriteLine($"Task: {taskName}; Game: {games.ToString("N")}, Score: {score.ToString("N")}");
+		}
+		void Pause() {
+			Console.Beep();
+			Console.ReadKey();
+		}
+
+		Task StartTask(string taskName) {
+			Action<object> pigdice = (object name) => { RunForBootcampRecord(name.ToString()); };
+			Task t = new Task(pigdice, taskName);
+			t.Start();
+			//t.Wait();
+			return t;
+		}
+		void StartTasks() {
+			Task t1 = StartTask("t1");
+			Task t2 = StartTask("t2");
+			while (!t1.IsCompleted && !t2.IsCompleted) {
+
+			}
+		}
+
 		static void Main(string[] args) {
-			(new Program()).Run2();
+			(new Program()).StartTasks();
 		}
 	}
 }
